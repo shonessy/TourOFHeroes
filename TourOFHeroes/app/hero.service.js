2f6be8867e37f546000b5d2e94ce0386951dbdb7
nemanja.filipovic <nemanja.el@hotmail.com>
2017-03-04 09:09:41 +0100

    Ch5 - Services

---
+++ TourOFHeroes/app/hero.service.js
@@ -0,0 +1,26 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@angular/core");
+const mock_heroes_1 = require("./mock-heroes");
+let HeroService = class HeroService {
+    getHeroes() {
+        return Promise.resolve(mock_heroes_1.HEROES);
+    }
+    getHeroesSlowly() {
+        return new Promise(resolve => {
+            // Simulate server latency with 2 second delay
+            setTimeout(() => resolve(this.getHeroes()), 2000);
+        });
+    }
+};
+HeroService = __decorate([
+    core_1.Injectable()
+], HeroService);
+exports.HeroService = HeroService;
+//# sourceMappingURL=hero.service.js.map
---
+++ TourOFHeroes/app/tests/1st.spec.js
@@ -0,0 +1,4 @@
+describe('1st tests', () => {
+    it('true is true', () => expect(true).toBe(true));
+});
+//# sourceMappingURL=1st.spec.js.map
